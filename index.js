6ca3215434749f033a0987f4663bad9e5d643d6f
William Dib <info@williamdib.com>
2024-11-17 16:43:36 -0500

    Added simple Express server to say hello

---
+++ index.js
@@ -0,0 +1,27 @@
+const
+  expressObj = require( 'express' ),
+  morganFn   = require( 'morgan'  ),
+  app        = expressObj(),
+  msgList    = [
+    'Welcome to TechWeek!',
+    'Hello from Montréal!',
+    'Bienvenue à TechWeek :)',
+  ]
+;
+
+app.use( morganFn( 'dev' ) );
+
+app.get( '/', function ( request, response ) {
+  response
+    .status( 200 )
+    .send( msgList[ Math.floor( Math.random() * msgList.length ) ] )
+  ;
+});
+
+let server = app.listen( '3000', function () {
+  let address_map = server.address();
+  console.log(
+    'Waiting to say hello on '
+    + address_map.address + ':' + address_map.port
+  );
+});
